374aa1f7eb2b0d84074666878bec0967748270d1
elenuco <elenuco36@gmail.com>
2023-02-15 14:08:23 -0600

    feat:

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("Hola mundo")
+var nombre= "JavaScript"
+nombre="PHP"
+var nombre = "SQL"
+console.log(nombre)
+let apellido ="Rodriguez"
+  apellido="Fernandez"
+//let apellido ="Hernandez"
+console.log(apellido)
+const nombre2= "Alberto"
+console.log(nombre2)
